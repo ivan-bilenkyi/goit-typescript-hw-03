66a328c920e68582f130cc03d8123260c75a823a
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2023-12-25 23:11:14 +0200

    create task

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ class Student {
   public age: number;
   public grade: string;
 
-  constructor(name: string, age: number, grade: string) {
+  constructor( name: string, age: number, grade: string) {
     this.name = name;
     this.age = age;
     this.grade = grade;
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Двері відчинені, Ви зайшли");
+    } else {
+      console.log("Двері зачинені. Потрібен ключ");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відчинені.");
+    } else {
+      console.log("Неправильний ключ, спробуйте інший ключ");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +60,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
